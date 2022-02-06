3d6920ea55955aff1d86bc27f66e0900f5f8defa
ne0massx <94698047+ne0massx@users.noreply.github.com>
2022-02-06 08:53:45 +0300

    Add files via upload

---
+++ activation/unpacked 22a16 beta/app/MainForm.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event buttonAlt.click-Left 
+     */
+    function doButtonAltClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button3.click-Left 
+     */
+    function doButton3ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button4.click-Left 
+     */
+    function doButton4ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+}
