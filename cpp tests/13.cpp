5b296899d3ef8532ef1dba69eadbef987e2dfdf9
ne0massx <94698047+ne0massx@users.noreply.github.com>
2022-05-22 15:38:24 +0300

    Delete cpp tests directory

--- cpp tests/13.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-main()
-{
-	string a, b, c;
-	cin>>a>>b>>c;
-	cout<<a[0]<<". "<<b[0]<<". "<<c;
-}
--- cpp tests/9.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-main()
-{
-	string a, b, c;
-	cin>>a>>b>>c;
-	cout<<c<<" "<<a[0]<<"."<<b[0]<<".";
-}
